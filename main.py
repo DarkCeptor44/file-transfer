a7eddeda46a20731d056230b002046f0dfff8fef
129fc5e32c631b5007cb2cd8d45d416002b42bab <16278483+DarkCeptor44@users.noreply.github.com>
2020-04-16 23:03:57 -0300

    Fixed CLI and many things

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pathlib
 from tqdm import tqdm
 from tkinter import Tk, filedialog
 from argparse import ArgumentParser
+from colorama import init, Fore
 
 SEPARATOR = '<SEPARATOR>'
 BUFFER_SIZE = 4096
@@ -67,10 +68,10 @@ class Client:
         # size = os.path.getsize(file_to_send)
         size = file_to_send.stat().st_size
         s = socket.socket()
-        s.settimeout(1)
-        print(f'[*] Connecting to {self.host}:{self.port}')
+        # s.settimeout(1)
+        print(f'{Fore.YELLOW}[*] Connecting to {self.host}:{self.port}{Fore.RESET}')
         s.connect((self.host, self.port))
-        print('[+] Connected.')
+        print(f'{Fore.GREEN}[+] Connected.{Fore.RESET}')
 
         s.send(f'{filename}{SEPARATOR}{size}'.encode())
         Client.actually_send(s, file_to_send)
@@ -78,7 +79,7 @@ class Client:
     @staticmethod
     def actually_send(s: socket.socket, file: pathlib.WindowsPath):
         size = file.stat().st_size
-        progress = get_progress_bar(f'Sending {file.name}', size)
+        progress = get_progress_bar(f'{Fore.GREEN}Sending {file.name}{Fore.RESET}', size)
         with open(str(file.absolute()), 'rb') as f:
             for _ in progress:
                 bytes_read = f.read(BUFFER_SIZE)
@@ -102,19 +103,19 @@ class Server:
         s = socket.socket()
         s.bind((self.host, self.port))
         s.listen(5)
-        print(f'[*] Listening as {self.host}:{self.port}')
+        print(f'{Fore.GREEN}[*] Listening as {self.host}:{self.port}{Fore.RESET}')
 
         client_socket, address = s.accept()
-        print(f'[+] {address} is connected.')
+        print(f'{Fore.BLUE}[+] {address} is connected.{Fore.RESET}')
 
         received = client_socket.recv(BUFFER_SIZE).decode()
         filepath, filesize = received.split(SEPARATOR)
         file = pathlib.Path(filepath)
-        Server.actually_receive(s, client_socket, int(filesize))
+        Server.actually_receive(s, client_socket, file, int(filesize))
 
     @staticmethod
     def actually_receive(s: socket.socket, cs: socket.socket, file: pathlib.WindowsPath, size: int):
-        progress = get_progress_bar(f'Receiving {file.name}', size)
+        progress = get_progress_bar(f'{Fore.GREEN}Receiving {file.name}{Fore.RESET}', size)
         with open(str(ask_where_to_save().absolute()), 'wb') as f:
             for _ in progress:
                 bytes_read = cs.recv(BUFFER_SIZE)
@@ -130,19 +131,28 @@ class Server:
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='File Transfer Utility')
-    parser.add_argument('client', required=True, const=True, type=bool, help='Start client', nargs='?')
-    parser.add_argument('server', required=True, const=True, type=bool, help='Start server', nargs='?')
+    parser.add_argument('operation', help='Client or Server', nargs='?')
+    parser.add_argument('--host', help='IP Address of the host', type=str)
     args = parser.parse_args()
 
     try:
-        if args.client:
-            print('Starting client...')
-        elif args.server:
-            print('Starting server...')
-        else:
-            print('Unsupported option')
+        if str(args.operation).lower() == 'client':
+            if args.host is not None and args.host != '':
+                print('Starting client...')
 
-        # client = Client('192.168.0.144')
-        # client.send(ask_for_file())
+                client = Client(args.host)
+                client.send(ask_for_file())
+            else:
+                print(f'{Fore.RED}Invalid Host{Fore.RESET}')
+                parser.print_usage()
+        elif str(args.operation).lower() == 'server':
+            print('Starting server...')
+
+            server = Server()
+            server.receive()
+        else:
+            parser.print_help()
+    except KeyboardInterrupt:
+        print('Exitting now...')
     except Exception as e:
-        print(f'Error: {str(e)}')
+        print(f'{Fore.RED}Error: {str(e)}{Fore.RESET}')
